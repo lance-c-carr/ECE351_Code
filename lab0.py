01eeeafbaafad4c71df95f2b08f07e20c990216f
lance-c-carr <60166521+lance-c-carr@users.noreply.github.com>
2020-01-21 20:36:58 -0800

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,14 @@
+################################################################
+#                                                              #
+# Lance Carr                                                   #
+# ECE 351-53                                                   #
+# Lab No.                                                      #
+# Due Date                                                     #
+# Notes                                                        #
+################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy  as sig
+import pandas as con
+import control as pd               
